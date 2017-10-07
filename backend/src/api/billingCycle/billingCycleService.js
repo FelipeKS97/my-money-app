d8ffb8f6da00035913fadf26245cac6cdc6851ee
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2017-10-07 01:57:58 -0300

    Summary route added, API config almost done

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -8,16 +8,31 @@ BillingCycle.route('count', (req, res, next) => {
    BillingCycle.count((error, value) => {
     
     if(error) {
-        
         res.status(500).json({errors: [error]})
-        
-    }   else {
-        
-            res.json({value})
-        
+
+    } else {
+        res.json({value})
+
+    }    
+   })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+
+    BillingCycle.aggregate({
+        $project:{ $credit:{"$credits.value"}, $debt:{"$debts.value"}}
+    },{
+        $group:{_id:null, $credit:{$sum:"$credit"}, $debt:{$sum:"$debt"}}
+    },{
+        $project:{ _id:0, credit:1, debt:1}
+    }, (error, result) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || { credit:0, debit:0 })
         }
-    
-   })
-   })
+    }
+)
+})
 
 module.exports = BillingCycle
