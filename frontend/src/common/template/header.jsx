cdfc35a351754fec353385faedeb363534767914
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2017-10-07 13:10:58 -0300

    Header.jsx component started, using LTE template

---
+++ frontend/src/common/template/header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+export default props => (
+ 
+<header className='main-header'>
+    <a href='/#/' className='logo'> 
+    
+        <span className='logo-mini'>
+            <b>My</b> <i className='fa fa-money'></i>
+        </span>
+    
+        <span className='logo-lg'>
+            <b> My</b> Money <i className='fa fa-money'></i>
+        </span>
+ 
+    </a>
+ 
+<nav className='navbar navbar-static-top'>
+    <a 
+        href 
+        className='sidebar-toggle' 
+        data-toggle='offcanvas'
+    >
+    </a>
+</nav>
+ 
+</header>
+)
