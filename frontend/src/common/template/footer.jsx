d8963f35889da7a4d567e9dbfef5f8b1488c9cdc
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2017-11-02 21:33:38 -0300

    Adding footer and some custom css.

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export default props => (
+
+    <footer className='main-footer'>
+
+        <strong>
+            My Github:
+
+                <a href='https://github.com/FelipeKS97' target='_blank'> Felipe Cavalcanti </a>.
+
+        </strong>
+
+    </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,13 +3,20 @@ import '../common/template/dependencies'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
 
 export default props => (
- 
-<div className='wrapper'>
 
-    <Header />
-    <SideBar />
+    <div className='wrapper'>
 
-</div>
+        <Header />
+        <SideBar />
+
+        <div className='content-wrapper'>
+            <h1>Content</h1>
+        </div>
+
+        <Footer />
+
+    </div>
 )
